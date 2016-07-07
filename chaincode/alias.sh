5aab571990546b5bf1ca1e085cd2665914b67069
Oleg Abdrashitov <olegabu73@gmail.com>
2016-07-07 18:29:08 -0400

    vp2

--- chaincode/alias.sh
+++ chaincode/alias.sh
@@ -2,3 +2,4 @@
 alias ssh-vp='ssh ubuntu@54.175.150.159 -i ~/insurance-fronting.pem'
 alias ssh-vp0='ssh ubuntu@54.234.129.66 -i ~/insurance-fronting.pem'
 alias ssh-vp1='ssh ubuntu@54.234.27.243 -i ~/insurance-fronting.pem'
+alias ssh-vp2='ssh ubuntu@54.175.231.18 -i ~/insurance-fronting.pem'
