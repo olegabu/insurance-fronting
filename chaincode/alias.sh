c709446ed5776cd7ec106e7b6fc6cd50a0c55f8a
Oleg Abdrashitov <olegabu73@gmail.com>
2016-07-07 17:31:25 -0400

    help scripts

---
+++ chaincode/alias.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+alias ssh-vp ssh ubuntu@54.175.150.159 -i ~/insurance-fronting.pem
+alias ssh-vp0 ssh ubuntu@54.234.129.66 -i ~/insurance-fronting.pem
+alias ssh-vp1 ssh ubuntu@54.234.27.243 -i ~/insurance-fronting.pem
